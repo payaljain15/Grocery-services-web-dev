3c1aed88d9e73531055cfb98d9a93d341e4a0b62
Payal jain <201210056@nitdelhi.ac.in>
2022-01-14 00:54:51 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Emptineful123*",
+    // database: "grocery",
+  });
+  
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+
+
+app.get('/createdb' , (req,res) => {
+    let sql = 'CREATE DATABASE grocery';
+    db.query(sql , (err,result) => {
+        if (err) throw err;
+        res.send('database created successfully');
+
+    });
+});
+
+app.listen('3000' , () =>{ 
+    console.log('Hey there! port 3000');
+} );
