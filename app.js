06e001603c2fbbb521ea658f214ea5affdcb139a
Payal jain <201210056@nitdelhi.ac.in>
2022-06-09 14:12:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,29 @@ const mysql = require('mysql');
 
 const app = express();
 var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Emptineful123*",
-    // database: "grocery",
-  });
-  
-  db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-
-
-
-app.get('/createdb' , (req,res) => {
-    let sql = 'CREATE DATABASE grocery';
-    db.query(sql , (err,result) => {
-        if (err) throw err;
-        res.send('database created successfully');
-
-    });
+  host: "localhost",
+  user: "root",
+  password: "Emptineful123*",
+  database: "grocery",
 });
 
-app.listen('3000' , () =>{ 
-    console.log('Hey there! port 3000');
-} );
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE grocery';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send('database created successfully');
+
+  });
+});
+
+app.listen('3000', () => {
+  console.log('Hey there! port 3000');
+});
