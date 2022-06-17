e4d04be8883596a0d37b03ddf48c4dbf0953fea9
SATZEL02 <gargdev89@gmail.com>
2022-06-17 21:00:14 +0530

    Update select.js

--- select.js
+++ select.js
@@ -29,7 +29,6 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    //Select all customers and return the result object:
     con.query("SELECT * FROM grocery.games", function (err, result, fields) {
       if (err) throw err;
       console.log(result);
