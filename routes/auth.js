56e417bb3e86549da623223be582c17cd47f1e04
Payal jain <201210056@nitdelhi.ac.in>
2022-06-23 15:53:18 +0530

    gg

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const authController = require('../controllers/auth');
 const authcontroller = require('../controllers/select');
 const router = express.Router();
 
-router.post('/register',authController.register)
-router.post('/login', authController.login)
-router.post('/forgot_password' , authController.forgot_password)
-router.post('/otp' , authController.otp)
+router.post('/register',authController.register);
+router.post('/login', authController.login);
+router.post('/forgot_password' , authController.forgot_password);
+router.post('/otp' , authController.otp);
 module.exports = router;
